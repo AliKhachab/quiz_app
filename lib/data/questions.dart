435c874a105ccab27e87ddd72315b3bcd13c8c7e
AliKhachab <a.khachab620@gmail.com>
2025-10-06 16:51:00 -0400

    more questions

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -37,5 +37,50 @@ const QUESTIONS = [
       "You are too soft, Sengoku",
       "Do you want to get donuts Koby?"
     ]
-  ) 
+  ),
+  QuizQuestion(
+    text: "What is Jinbei's role in the Straw Hat crew?",
+    answers: [
+      "Helmsman",
+      "Cook",
+      "Swordsman",
+      "Amazon delivery"
+    ],
+  ),
+  QuizQuestion(
+    text: "What did Sanji say to Charlotte Pudding to stun her for a few seconds?",
+    answers: [
+      "What a beautiful third eye...",
+      "Damn you got a LOOKER up there",
+      "I heard what you said in the chateau",
+      "I reject this marriage",
+    ],
+  ),
+  QuizQuestion(
+    text: "Who was the man Yamato aspired to be like?",
+    answers: [
+      "Kozuki Oden",
+      "Kin'emon",
+      "Portgas D. Ace",
+      "Roronoa Zoro",
+    ],
+  ),
+  QuizQuestion(
+    text: "What is the name of the sword that Roronoa Zoro received from Kuina?",
+    answers: [
+      "Wado Ichimonji",
+      "Shusui",
+      "Enma",
+      "Sandai Kitetsu",
+    ],
+  ),
+  QuizQuestion(
+    text: "What is the name of the organization in the marines that Koby is a part of?",
+    answers: [
+      "SWORD",
+      "SHIELD",
+      "CP0",
+      "G-5",
+    ],
+  )
 ];
